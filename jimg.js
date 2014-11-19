43bf25fdce62c8587ea109c566fee26c2c91ed6d
katchou <jean.francois.jung@gmail.com>
2014-11-19 19:23:41 +0100

    still need timer

--- jimg.js
+++ jimg.js
@@ -1,5 +1,7 @@
 (function() {
-    var files = [],
+    var file_list = null,
+        current_index = 0,
+        duration = 10000,
         img = document.querySelector('#img'),
         files = document.querySelector('#files'),
         time = document.querySelector('#time'),
@@ -8,37 +10,56 @@
         next = document.querySelector('#next');
 
     function init() {
-        files.addEventListener('change', getFiles);
-        time.addEventListener('click', getTime);
+        files.addEventListener('change', setFiles);
+        time.addEventListener('click', setTime);
         previous.addEventListener('click', previousImage);
         next.addEventListener('click', nextImage);
         play.addEventListener('click', runShow);
     }
 
-    function getFiles(ev) {
-        files = ev.target.files;
+    function setFiles(ev) {
+        file_list = ev.target.files;
         ev.preventDefault();
-        runShow();
     }
 
-    function getTime(ev) {
+    function setTime(ev) {
+        time_input = document.createElement('input');
+        time_input.setAttribute('type', 'time');
+        time_input.setAttribute('id', 'time_input');
+        time_input.addEventListener(
+            'keypress', 
+            function(ev) {
+                if (ev.keyCode == 13) {
+                    if (time_input.value != '' && time_input.validity.valid) {
+                        duration = time_input.valueAsNumber/60;
+                        time_input.parentNode.removeChild(time_input);
+                    }
+                }
+            },
+            true);
+        time.parentNode.insertBefore(time_input, time);
+        time_input.focus();
     }
 
     function previousImage(ev) {
+        current_index = (file_list.length+current_index-1)%file_list.length;
+        showImage(current_index);
     }
 
     function nextImage(ev) {
+        current_index = (current_index+1)%file_list.length;
+        showImage(current_index);
     }
 
-    function runShow( ev ) {
-        window.URL = window.URL || window.webkitURL;
-        var file = files.shift();
-        var blob = window.URL.createObjectURL(file);
-        showImage(blob);
+    function runShow(ev) {
+        if (file_list) {
+            showImage(current_index);
+            window.setInterval(nextImage, duration);
+        }
     }
 
-    function showImage(blob) {
-        img.src = blob;
+    function showImage(index) {
+        img.src = window.URL.createObjectURL(file_list.item(index));
     }
 
     init();
