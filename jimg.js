aa560caf33082250c0aa8b2dcb99cb2316b47645
m0nsterinside <monsterinside.contact@gmail.com>
2014-11-30 11:47:34 +0100

    now working

--- jimg.js
+++ jimg.js
@@ -1,67 +1,72 @@
 (function() {
     var file_list = null,
-        timer = null,
+        intid = null,
         current_index = - 1,
-        duration = 0,
-        current_time = 10,
+        duration = 3,
+        current_time = 0,
+
         menu = document.querySelector('#menu'),
-        img = document.querySelector('#img'),
-        nav = document.querySelector('#nav'),
+        imageView = document.querySelector('#imageView'),
+        image = document.querySelector('#image'),
         files = document.querySelector('#files'),
         files_label = document.querySelector('#files_label'),
-        count = document.querySelector('#count'),
+        files_input = document.querySelector('#files_input'),
+
         time = document.querySelector('#time'),
-        time_input = document.querySelector('#time_input'),
-        time_bubble = document.querySelector('#time_bubble'),
+        time_form = document.querySelector('#time_form'),
+        time_input_label = document.querySelector('#time_input_label'),
         time_input_box_min = document.querySelector('#time_input_box_min'),
         time_input_box_sec = document.querySelector('#time_input_box_sec'),
+        time_bubble = document.querySelector('#time_bubble'),
+        timer = document.querySelector('#timer'),
+
         play = document.querySelector('#play'),
+        play_label = document.querySelector('#play_label'),
         pause = document.querySelector('#pause'),
         previous = document.querySelector('#previous'),
         next = document.querySelector('#next');
 
     function init() {
-        files.addEventListener('change', setFiles);
-        time.addEventListener('click', setTime);
-        time_input.addEventListener('submit', setTimeValue);
+        files_input.addEventListener('change', setFiles);
+        time_input.addEventListener('click', showTimeBubble);
+        timer.addEventListener('click', showTimeBubble);
+        time_form.addEventListener('submit', setTimeValue);
         time_input_box_min.addEventListener('keydown', setTimeValueSanitize);
         time_input_box_sec.addEventListener('keydown', setTimeValueSanitize);
         time_input_box_min.addEventListener('keyup', setTimeValueSanitize);
         time_input_box_sec.addEventListener('keyup', setTimeValueSanitize);
         time_input_box_min.addEventListener('keyup', setTimeValueHelperNavigateMin);
         time_input_box_sec.addEventListener('keydown', setTimeValueHelperNavigateSec);
+        // image.addEventListener('mouseenter', showPause);
+        // image.addEventListener('mouseleave', hidePause);
         previous.addEventListener('click', previousImage);
         next.addEventListener('click', nextImage);
         play.addEventListener('click', runShow);
-        img.addEventListener('load', resizeImage);
     }
 
     function setFiles(ev) {
         file_list = ev.target.files;
-        files_label.innerHTML = current_index + 1 + "/" + file_list.length;
+        showFiles();
         playMode();
         ev.preventDefault();
     }
 
     function playMode() {
-        play.style.display = "block";
-        menu.setAttribute('play', '1');
-        previous.style.display = "block";
-        next.style.display = "block";
+        if (file_list && duration) {
+            imageView.style.display = "block";
+            menu.setAttribute('play', '1');
+            previous.style.display = "block";
+            next.style.display = "block";
+            play.style.display = "flex";
+        }
     }
 
-    function setTime(ev) {
+    function showTimeBubble(ev) {
+        time_input_label.innerHTML = 'OK';
+        time_input_label.style.fontSize = "46px";
+        time_input_label.style.display = "table-cell";
+        timer.style.display = "none";
         time_bubble.style.display = "block";
-        time_input_box_min.addEventListener('blur', function() {
-            if (isNaN(time_input_box_min.value)) {
-                time_input_box_min.focus();
-                return;
-            }
-            if (isNaN(time_input_box_sec.value)) {
-                time_input_box_sec.focus();
-                return;
-            }
-        });
         time.setAttribute('active', '1');
         time_input_box_min.placeholder = pad_time(Math.floor(duration / 60));
         time_input_box_sec.placeholder = pad_time(Math.floor(duration % 60));
@@ -79,131 +84,247 @@
 
     function setTimeValue(ev) {
         ev.preventDefault();
-        duration = parseInt(time_input_box_min.value) * 60 + parseInt(time_input_box_sec.value);
-        if (!isNaN(duration)){
-            time_input.reset();
+        if (time_input_box_min.value || time_input_box_sec.value) {
+            time_input_box_min.value = time_input_box_min.value ? time_input_box_min.value : "0";
+            time_input_box_sec.value = time_input_box_sec.value ? time_input_box_sec.value : "0";
+            duration = parseInt(time_input_box_min.value) * 60 + parseInt(time_input_box_sec.value);
+            time_form.reset();
             time_bubble.style.display = "none";
             time.removeAttribute('active');
+            current_time = duration;
             showTimer();
         } else {
             duration = 0;
         }
+        playMode();
         return false;
     }
 
     function setTimeValueSanitize(ev) {
         ev.target.value = ev.target.value.replace(/[^0-9]/, '');
-        return /[0-9]/.test(String.fromCharCode(ev.keyCode));
     }
 
     function setTimeValueHelperNavigateMin(ev) {
-        if (ev.target.value.length == 2) {
+        if (ev.target.value.length == 2 || ev.keyCode == 39) {
             time_input_box_sec.focus();
         }
     }
 
     function setTimeValueHelperNavigateSec(ev) {
-        if (ev.target.value.length == 0 && ev.keyCode == 8) {
+        if ((ev.target.value.length == 0 && ev.keyCode == 8)
+            || ev.keyCode == 37
+        ) {
             time_input_box_min.focus();
         } 
     }
 
     function previousImage(ev) {
         if (file_list) {
+            resetTimer();
             current_index = (file_list.length + current_index - 1) % file_list.length;
-            showImage();
+            runShow();
         }
     }
 
     function nextImage(ev) {
         if (file_list) {
+            resetTimer();
             current_index = (current_index + 1) % file_list.length;
-            showImage();
+            runShow();
         }
+    }
+
+    function showFiles() {
+        files_label.innerHTML = current_index + 1 + '/' + file_list.length; 
     }
 
     function showTimer() {
-        canvas = document.createElement('canvas');
-        canvas.height = 112;
-        canvas.width = 112;
-        time.appendChild(canvas);
-        context = canvas.getContext('2d');
-        centerX = canvas.width / 2;
-        centerY = canvas.height / 2;
-        radius = 51;
-        draw();
-    // animate();
+        time.querySelector('label').style.display = "none";
+        timer.style.display = "block";
+
+        var context = timer.getContext('2d'),
+            centerX = timer.width / 2,
+            centerY = timer.height / 2,
+            radius = 50,
+            min_str = pad_time(Math.floor(current_time / 60)),
+            sec_str = pad_time(Math.floor(current_time % 60)),
+            completion = 1 - current_time / Math.min(90, duration);
+
+            context.clearRect(0, 0, timer.width, timer.height);
+
+        if (current_time > 90 ) {
+
+            context.beginPath();
+            context.arc(centerX, centerY, radius, Math.PI * (-0.5), Math.PI * (1.5), true);
+            context.lineWidth = 10;
+            context.strokeStyle = 'rgb(241,101,76)';
+            context.fillStyle = 'rgb(241,101,76)';
+
+            context.stroke();
+            context.fill();
+
+            context.font = 'normal 26px novecento_wide_bookbold';
+            context.textAlign = 'center';
+            context.fillStyle = '#FFF';
+            context.fillText(min_str + ":" + sec_str, centerX, centerY + 10);
+
+        } else {
+            // duration = 100;
+            context.beginPath();
+            context.arc(centerX, centerY, radius, Math.PI * 1.5, Math.PI * (2 * completion - 0.5), false);
+            context.lineWidth = 10;
+            context.strokeStyle = '#000';
+            context.stroke();
+
+            context.beginPath();
+            context.arc(centerX, centerY, radius, Math.PI * 1.5, Math.PI * (2 * completion - 0.5), true);
+            context.lineWidth = 10;
+            context.strokeStyle = 'rgb(241,101,76)';
+            context.stroke();
+
+            context.font = 'normal 50px novecento_wide_bookbold';
+            context.textAlign = 'center';
+            context.fillStyle = '#FFF';
+            context.fillText(current_time, centerX, centerY + 10);
+
+            context.fillStyle = 'rgb(241,101,76)';
+            context.font = 'normal 18px novecento_widelight';
+            context.fillText('SEC', centerX, centerY + 32);
+
+        }
     }
 
-    // function animate() {
-    //   setTimeout(function() {
-    //     window.requestAnimationFrame(animate);
-    //     duration++;
-    //     draw();
-    //   }, 1000);
-    // }
-
-    function draw() { 
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.beginPath();
-      context.arc(centerX, centerY, radius, Math.PI * 1.5, Math.PI * (duration / 50 - 0.5), false);
-      context.lineWidth = 8;
-      context.strokeStyle = '#000';
-      context.stroke();
-
-      context.beginPath();
-      context.arc(centerX, centerY, radius, Math.PI * 1.5, Math.PI * (duration / 50 - 0.5), true);
-      context.lineWidth = 8;
-      context.strokeStyle = 'rgb(241,101,76)';
-      context.stroke();
-
-      context.font = 'normal 40pt novecento_wide_bookbold';
-      context.textAlign = 'center';
-      var width = context.measureText(duration).width;
-      context.fillText(duration, centerX, centerY);
-
-      context.font = '20pt Calibri';
-      context.fillText('%', centerX , centerY + 30);
-    }
-
-    function runShow(ev) {
+    function runShow() {
+        if (current_time == 0) {
+            current_time = duration;
+        }
         if (current_index < 0) {
             current_index = 0;
         }
-        if (file_list && !timer) {
+        if (file_list && !intid) {
+            // play.style.display = "none";
+            // play.removeAttribute('pause');
+            play.setAttribute('play', '1');
             showTimer();
             showImage();
-            timer = window.setInterval(updateTimer, 1000);
+            showFiles();
+            intid = window.setInterval(updateTimer, 1000);
+            return;
+        }
+        if (intid) {
+            pauseShow();
         }
     }
 
-    function pauseShow(ev) {
-        if (timer) {
-            window.clearInterval(timer);
-            timer = null;
-            togglePlayPause();
-        }
+    function pauseShow() {
+        stopTimer();
+        play.removeAttribute('play');
+        play.setAttribute('pause', '1');
+
     }
+
+    function stopTimer() {
+        window.clearInterval(intid);
+        intid = null;
+
+    }
+
+    function resetTimer() {
+        stopTimer();
+        current_time = duration;
+    }
+
+    function stopShow() {
+
+    }
+
 
     function updateTimer() {
-        current_time--;
-        if (current_time == 0) {
+        // current_time--;
+        if (--current_time <= 0) {
             current_time = duration;
+            if (current_index + 1 == file_list.length) {
+                play.removeAttribute('play');
+                resetTimer();
+                // showTimer();            
+                imageView.style.backgroundImage = "none";
+                return;
+            }
             nextImage();
             showImage();
+            files_label.innerHTML = current_index + 1 + '/' + file_list.length;
+        }
+        showTimer();
+        if (current_time < 5) {
+            document.querySelector('#bip').play();
         }
         // time_display.innerHTML = current_time;
     }
 
+    // function showImage() {
+    //     image.onload = function () {
+    //         if (image.naturalHeight > image.naturalWidth) {
+    //             var computedHeight = Math.min(parseInt(window.getComputedStyle(imageView).height), image.naturalHeight);
+    //             var ratio = computedHeight / image.naturalHeight;
+    //             image.style.height = computedHeight + "px";
+    //             image.style.width = image.naturalWidth * ratio + "px";
+    //             play.style.height = computedHeight + "px";
+    //             play.style.width = image.naturalWidth * ratio + "px";
+    //         } else {
+    //             var computedwidth = Math.min(parseInt(window.getComputedStyle(imageView).width), image.naturalWidth);
+    //             var ratio = computedwidth / image.naturalWidth;
+    //             image.style.width = computedwidth + "px";
+    //             image.style.height = image.naturalHeight * ratio + "px";
+    //             var ratio = computedwidth / image.naturalWidth;
+    //             play.style.width = computedwidth + "px";
+    //             play.style.height = image.naturalHeight * ratio + "px";
+    //         }
+    //     }
+    //     image.src = window.URL.createObjectURL(file_list.item(current_index));
+    // }
+
+
     function showImage() {
-        img.src = window.URL.createObjectURL(file_list.item(current_index));
+        var image = new Image();
+        image.onload = function () {
+            if (image.naturalHeight > image.naturalWidth) {
+                var computedHeight = Math.min(parseInt(window.getComputedStyle(play).height), image.naturalHeight);
+                var ratio = computedHeight / image.naturalHeight;
+                imageView.style.height = computedHeight + "px";
+                imageView.style.width = image.naturalWidth * ratio + "px";
+                // play.style.height = computedHeight + "px";
+                // play.style.width = image.naturalWidth * ratio + "px";
+            } else {
+                var computedwidth = Math.min(parseInt(window.getComputedStyle(play).width), image.naturalWidth);
+                var ratio = computedwidth / image.naturalWidth;
+                imageView.style.width = computedwidth + "px";
+                imageView.style.height = image.naturalHeight * ratio + "px";
+                var ratio = computedwidth / image.naturalWidth;
+                // play.style.width = computedwidth + "px";
+                // play.style.height = image.naturalHeight * ratio + "px";
+            }
+            imageView.style.backgroundImage = 'url(' + this.src + ')';
+        }
+        image.src = window.URL.createObjectURL(file_list.item(current_index));
+    }
+    function showPause() {
+        // imageView.style.zIndex = 2;
+        // play_label.style.backgroundImage = 'url(assets/images/btn_stop.png)';
+        play.style.backgroundColor = 'rgba(0,0,0,0.4)';
+        play.style.display = "flex";
+        // play.style.opacity = '0.9';
+        // play.style.backgroundImage = 'url(assets/images/btn_stop.png)';
+        // play.style.display = "block";
     }
 
-    function resizeImage(ev) {
-        img.style.height = ev.target.naturalHeight + "px";
-        img.style.width = ev.target.naturalWidth + "px";
-        // img.style.boxShadow = "inset 0px 0px 150px 150px #000";
-        files_label.innerHTML = current_index + 1 + '/' + file_list.length; 
+    function hidePause() {
+        // imageView.style.zIndex = 2;
+        // play_label.style.backgroundImage = 'url(assets/images/btn_stop.png)';
+        // play.style.backgroundColor = 'rgba(0,0,0,0.4)';
+        play.style.display = "none";
+        // play.style.opacity = '0.9';
+        // play.style.backgroundImage = 'url(assets/images/btn_stop.png)';
+        // play.style.display = "block";
     }
 
     init();
